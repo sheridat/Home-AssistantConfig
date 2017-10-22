cfc0f39078e55b8809ad5d3eeb4392e1f362867d
sheridat <mrterrysheridan@gmail.com>
2017-10-22 16:30:44 +0000

    initial

---
+++ README.md
@@ -0,0 +1,2 @@
+# Home-AssistantConfig
+Backup of my home assistant config
---
+++ copy.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cp automations.yaml /mnt/usbstorage/HomeAssistant/automations.yaml
+cp configuration.yaml /mnt/usbstorage/HomeAssistant/configuration.yaml
+cp groups.yaml /mnt/usbstorage/HomeAssistant/groups.yaml
